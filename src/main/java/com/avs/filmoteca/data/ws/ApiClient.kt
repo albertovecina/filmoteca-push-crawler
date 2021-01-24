3ce49b63ea842d5b5235df282a4322387c433936
Alberto Vecina Sánchez <avecina@idealista.com>
2021-01-24 21:33:31 +0100

    *[Network] Added basic auth interceptor

--- src/main/java/com/avs/filmoteca/data/ws/ApiClient.kt
+++ src/main/java/com/avs/filmoteca/data/ws/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.avs.filmoteca.data.ws
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -7,19 +8,34 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object ApiClient {
 
-    val filmotecaInterface: FilmotecaInterface
-        get() = getRetrofit(Environment.FILMOTECA_URL).create(FilmotecaInterface::class.java)
-
-    val backendInterface: BackendInterface
-        get() = getRetrofit(Environment.BACKEND_URL).create(BackendInterface::class.java)
-
-    val fcmInterface: FCMInterface
-        get() = getRetrofit(Environment.FIREBASE_URL).create(FCMInterface::class.java)
-
-    private fun getRetrofit(url: String): Retrofit {
-        return Retrofit.Builder().baseUrl(url).addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create()).build()
+    val filmotecaInterface: FilmotecaInterface by lazy {
+        getRetrofit(Environment.WEB).create(FilmotecaInterface::class.java)
     }
+
+    val backendInterface: BackendInterface by lazy {
+        getRetrofit(Environment.BACKEND).create(BackendInterface::class.java)
+    }
+
+    val fcmInterface: FCMInterface by lazy {
+        getRetrofit(Environment.FIREBASE).create(FCMInterface::class.java)
+    }
+
+    private val basicAuthHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(BasicAuthInterceptor())
+            .build()
+    }
+
+    private fun getRetrofit(environment: Environment): Retrofit =
+        Retrofit.Builder().apply {
+            baseUrl(environment.endpoint)
+
+            if (environment == Environment.BACKEND)
+                client(basicAuthHttpClient)
+
+            addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            addConverterFactory(ScalarsConverterFactory.create())
+            addConverterFactory(GsonConverterFactory.create())
+        }.build()
 
 }
