d328075a1583ed5e26f2f1b3519bbdbe80acca74
Alberto Vecina Sánchez <avecina@idealista.com>
2021-02-20 21:49:20 +0100

    *Implemented execution by region

--- src/main/java/com/avs/filmoteca/data/ws/FilmotecaInterface.kt
+++ src/main/java/com/avs/filmoteca/data/ws/FilmotecaInterface.kt
@@ -1,12 +1,11 @@
 package com.avs.filmoteca.data.ws
 
 import retrofit2.http.GET
-import retrofit2.http.Url
 import rx.Observable
 
 interface FilmotecaInterface {
 
-    @get:GET("/es/webs-municipales/filmoteca/agenda/folder_listing")
-    val moviesListHtmlObservable: Observable<String>
+    @GET("/es/webs-municipales/filmoteca/agenda/folder_listing")
+    fun getMoviesListHtmlObservable(): Observable<String>
 
 }
