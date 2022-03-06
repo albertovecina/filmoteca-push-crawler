aa16462b88904cba40828ccf8fa757f693230486
Alberto Vecina Sánchez <albertovecina@gmail.com>
2022-03-06 17:33:04 +0100

    Version code 1.1

--- src/main/java/com/avs/filmoteca/data/domain/Environment.kt
+++ src/main/java/com/avs/filmoteca/data/domain/Environment.kt
@@ -1,13 +1,10 @@
 package com.avs.filmoteca.data.domain
 
-sealed class Environment(val name: String) {
-
-    val value: String
-        get() = System.getenv(name)
+sealed class Environment(val name: String, val value: String = System.getenv(name)) {
 
     object BasicAuthUser : Environment("BASIC_AUTH_USER")
     object BasicAuthPassword : Environment("BASIC_AUTH_PASSWORD")
     object FirebaseApiKey : Environment("FIREBASE_API_KEY")
-    object EndpointWs : Environment("EndpointWs")
+    object EndpointWs : Environment("ENDPOINT_WS")
 
 }
